86d1d2ffe36ef6e756c9844ff4c01b3f514dbcfa
onionknight621 <onionknight621@gmail.com>
2023-06-15 20:53:13 +0300

    chore

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { LAT, LAT_B, CYR, CYR_B, LANGUAGES } from "./constants";
+import { LAT, LAT_CRI, CYR, CYR_CRI, LANGUAGES } from "./constants";
 
 function transliterate(text: string, lang: string = LANGUAGES.LAT): string {
   let fromLang = LAT;
-  let toLang = LAT_B;
+  let toLang = LAT_CRI;
   let result = text;
 
   if (lang.toUpperCase() === LANGUAGES.CYR) {
     fromLang = CYR;
-    toLang = CYR_B;
+    toLang = CYR_CRI;
   }
 
   for (let x = 0; x < fromLang.length; x++) {
